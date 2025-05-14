d276a20667d7673e6ce82c29deb05e8e36c8404c
efevans <efevans1@gmail.com>
2025-05-13 17:01:25 -0700

    Add minimal api with logging

--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
+using Amazon.CloudWatchLogs;
+using Routes;
+using Serilog;
+using Serilog.Sinks.AwsCloudWatch;
+using YaushServer.Url;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+ConfigureLogging(builder);
+
+builder.Services.AddScoped<ICreateUrlService, CreateUrlService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -16,12 +26,41 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.UseSerilogRequestLogging();
+
 app.MapControllers();
 
-app.MapGet("/", () => "Hello, World!");
+app.MapGet("/", async (ICreateUrlService createUrlService, ILogger<DefaultRoute> logger) =>
+{
+    logger.LogInformation("At Minimal Endpoint");
+    await createUrlService.CreateUrl("https://google.com");
+});
 
 app.Run();
+
+
+static void ConfigureLogging(WebApplicationBuilder builder)
+{
+    var client = new AmazonCloudWatchLogsClient();
+
+    builder.Logging.ClearProviders();
+    Serilog.ILogger logger = new LoggerConfiguration()
+        .WriteTo.Console()
+        .WriteTo.AmazonCloudWatch(
+            logGroup: $"{builder.Environment.EnvironmentName}/{builder.Environment.ApplicationName}",
+            logStreamPrefix: DateTime.Now.ToString("yyyyMMddHHmmssfff"),
+            cloudWatchClient: client
+            )
+        .CreateLogger();
+    builder.Services.AddSerilog(logger);
+}
+
+namespace Routes
+{
+    public class DefaultRoute { }
+}
---
+++ Url/CreateUrlService.cs
@@ -0,0 +1,18 @@
+﻿namespace YaushServer.Url
+{
+    public interface ICreateUrlService
+    {
+        Task CreateUrl(string url);
+    }
+
+    public class CreateUrlService(ILogger<CreateUrlService> logger) : ICreateUrlService
+    {
+        private readonly ILogger<CreateUrlService> _logger = logger;
+
+        public async Task CreateUrl(string url)
+        {
+            _logger.LogInformation("Create url for {host}", url);
+            await Task.Delay(1000);
+        }
+    }
+}
